36c48adfd7e9f0b0bdd17b6add3373cb0ca56389
Josh Kim <joshuak@cpp.edu>
2021-09-23 18:51:49 -0700

    Initial commit

---
+++ *.c
@@ -0,0 +1,13 @@
+// 1) Restaurant Bill
+// Include libraries
+// Declare and initialize variables: Salad, Soup, Sandwich, Pizza, Input
+// Randomly choose between one food out of the four for the base meal cost
+// Calculate tax and tip based off randomly chosen food, declare/initialize variables to hold calculations
+// Display meal cost, tax amount, tip amount, and total bill
+
+// 2) Number guessing game
+// Include libraries
+// Declare and initialize variables: Input, max number, etc.
+// Use if else to wait for input regarding 1,2,3
+// Code statements of 1,2,3
+// Create new branch and save last max number
